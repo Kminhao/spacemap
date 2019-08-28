ee829de448271627d5440bb2d746125862597509
Eduardo P de Sousa <edupsousa@gmail.com>
2019-08-28 17:50:20 -0300

    implementado middleware para log de requisições

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const debug = require('debug')('servidor');
 
 const app = express();
 
+app.use((req, res, next) => {
+  let url = req.url;
+  let method = req.method;
+  console.log(method + ' ' + url);
+  next();
+});
+
 app.use('/', (req, res, next) => {
   debug('Executando Middleware /');
   res.send('Olá no Navegador!');
