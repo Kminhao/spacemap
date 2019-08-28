6515cc07399d0b961aa4e45139e6ccc9c4995b68
Eduardo P de Sousa <edupsousa@gmail.com>
2019-08-28 12:42:21 -0300

    instalado express

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-const http = require('http');
-
+const express = require('express');
 const debug = require('debug')('servidor');
-debug('script em execução');
 
-const server = http.createServer((req, res) => {
-  debug('requisição recebida');
-  console.log('Olá no console!');
-  res.end('Ola no navegador!');
+const app = express();
+
+app.use('/', (req, res, next) => {
+  debug('Executando Middleware /');
+  res.send('Olá no Navegador!');
 });
 
-server.listen(3000, () => {
-  debug('servidor inicializado');
-  console.log(
-    'Para acessar o servidor abra o navegador e navegue para o endereço: http://localhost:3000/'
-  );
-});
+app.listen(3000);
