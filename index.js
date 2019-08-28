9be9ee0d6c15a24a73dcf513be40362b26351279
Eduardo P de Sousa <edupsousa@gmail.com>
2019-08-28 11:41:25 -0300

    exemplo biblioteca debug

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const http = require('http');
 
+const debug = require('debug')('servidor');
+debug('script em execução');
+
 const server = http.createServer((req, res) => {
+  debug('requisição recebida');
   console.log('Olá no console!');
   res.end('Ola no navegador!');
 });
 
 server.listen(3000, () => {
+  debug('servidor inicializado');
   console.log(
     'Para acessar o servidor abra o navegador e navegue para o endereço: http://localhost:3000/'
   );
