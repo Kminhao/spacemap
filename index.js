69a4c9a829eeed5439da2cf0ad333fd85474d23a
Eduardo P de Sousa <edupsousa@gmail.com>
2019-09-02 21:08:16 -0300

    criar nova prova e listar provas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -7,6 +8,29 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+var provas = [];
+
+app.post('/provas/nova', (req, res, next) => {
+  let novaProva = {
+    disciplina: req.body.disciplina,
+    professor: req.body.professor,
+    dataProva: req.body.dataProva,
+  };
+  provas.push(novaProva);
+  res.redirect('/provas');
+});
+
+app.get('/provas/nova', (req, res, next) => {
+  res.render('novaProva');
+});
+
+app.get('/provas', (req, res, next) => {
+  res.render('provas', {
+    'provas': provas,
+  });
+})
 
 app.get('/', (req, res, next) => {
   res.render('index', {
