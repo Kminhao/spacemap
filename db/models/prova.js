56080e73ecf6a492ff209258e2539694563cae86
Eduardo P de Sousa <edupsousa@gmail.com>
2019-09-06 00:50:13 -0300

    mostrar data formatada

--- db/models/prova.js
+++ db/models/prova.js
@@ -1,10 +1,19 @@
 'use strict';
+const moment = require('moment');
+
 module.exports = (sequelize, DataTypes) => {
   const Prova = sequelize.define('Prova', {
     id: { type: DataTypes.STRING, primaryKey: true, allowNull: false, autoIncrement: true },
     disciplina: { type: DataTypes.STRING, allowNull: false, },
     professor: { type: DataTypes.STRING, allowNull: false, },
     dataProva: { type: DataTypes.DATEONLY, allowNull: false, },
+    dataProvaLocal: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        let date = moment(this.getDataValue('dataProva'));
+        return date.format('DD/MM/YYYY');
+      }
+    }
   }, {});
   Prova.associate = function (models) {
     // associations can be defined here
